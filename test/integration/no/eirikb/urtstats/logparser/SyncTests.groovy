89571052600e0b9b45b3b887b3fddb5eba844417
eirikb@katze <eirikb@katze>
2009-07-20 19:14:13 +0200

    Worked on my machine

--- test/integration/no/eirikb/urtstats/logparser/SyncTests.groovy
+++ test/integration/no/eirikb/urtstats/logparser/SyncTests.groovy
@@ -13,7 +13,19 @@ class SyncTests extends GrailsUnitTestCase {
             "4    11  101 E agle Eye ^7              0 4:27960   859  8000\n" +
             "5    51  999 <[Supermarco2015]>^7   8950 5:27960    54551  8000\n" +
             "6     0   67 ^5deagle 5      ^7        0 6:27960     244  8000\n" +
-            "7     1   94 A|crusy|BoD^7           0 7:27960   54065  8000 "
+            "7     1   94 A|crusy|BoD^7           0 7:27960   54065  8000\n\n"
+
+    final def STATUS2 = "print\n" +
+            "map: ut4_riyadh\n" +
+            "num score ping name            lastmsg address               qport rate\n" +
+            "--- ----- ---- --------------- ------- --------------------- ----- -----\n" +
+            "0    15   58 EagleEye^7              0 0:27960   763  8000\n" +
+            "1     0  307 Andre^7                 0 1:27960    49453  8000\n" +
+            "2     5   62 dense^7                 0 2:61177       3761  8000\n" +
+            "3     1   46 bash^7                 50 3:27960     93  8000\n" +
+            "6    13  182 LOLMAN1996^7           50 6:27960     15237  8000\n" +
+            "8    29   75 NexT!^7                 0 8:27960   48888  8000\n" +
+            "11     0   62 deagle 5      ^7        0 11:27960     244  8000\n\n"
 
     protected void setUp() {
         super.setUp()
@@ -51,6 +63,23 @@ class SyncTests extends GrailsUnitTestCase {
         assertEquals "6:27960", map["6"].address
         assertEquals "A|crusy|BoD", map["7"].name
         assertEquals "7:27960", map["7"].address
+
+        map = new Sync().statusToMap(STATUS2)
+        assertEquals "EagleEye", map["0"].name
+        assertEquals "0:27960", map["0"].address
+        assertEquals "Andre", map["1"].name
+        assertEquals "1:27960", map["1"].address
+        assertEquals "dense", map["2"].name
+        assertEquals "2:61177", map["2"].address
+        assertEquals "bash", map["3"].name
+        assertEquals "3:27960", map["3"].address
+        assertEquals "LOLMAN1996", map["6"].name
+        assertEquals "6:27960", map["6"].address
+        assertEquals "NexT!", map["8"].name
+        assertEquals "8:27960", map["8"].address
+        assertEquals "deagle 5      ", map["11"].name
+        assertEquals "11:27960", map["11"].address
+
     }
 
     void testAddPlayer() {
