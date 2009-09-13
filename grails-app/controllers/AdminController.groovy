28b02586a64757651e0b37aefc1d4dae41fb51fb
eirikb@katze <eirikb@katze>
2009-09-13 15:55:20 +0200

    Old way of running parser - new try

--- grails-app/controllers/AdminController.groovy
+++ grails-app/controllers/AdminController.groovy
@@ -1,4 +1,28 @@
+import no.eirikb.urtstats.logparser.LogParser
+
 class AdminController {
 
-    def index = { }
+    private  static logParser
+
+    def index = {
+        def parserStatus = "Not running"
+        if (logParser != null) {
+            parserStatus = "Parser running"
+        }
+        [parserStatus : parserStatus]
+    }
+
+    def startParser = {   
+        if (logParser == null) {
+            logParser = new LogParser()
+            new Thread() {
+                while (true) {
+                    println "Thread run. " + this
+                    logParser.execute()
+                    Thread.sleep(1000)
+                }
+            }.start()
+        }
+        redirect(action:index)
+    }
 }
