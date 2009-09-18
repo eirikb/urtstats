20d7bf8e49c94040dd38da3f5a736294e66c69ae
eirikb@katze <eirikb@katze>
2009-09-18 21:27:26 +0200

    Removed the flood-test from TailTests

--- test/unit/no/eirikb/utils/tail/TailTests.groovy
+++ test/unit/no/eirikb/utils/tail/TailTests.groovy
@@ -51,24 +51,6 @@ class TailTests extends GrailsUnitTestCase {
         }
     }
 
-    void testFlood() {
-        createTestFile(testData)
-        def tail = new Tail(logFile, true)
-        Thread.start {
-            for (i in 0..100000) {
-                logFile << "test ${i}\n"
-                sleep(2)
-            }
-        }
-        Thread.start {
-            for (i in 0..100000) {
-                println tail.parse()
-                sleep(1)
-            }
-        }
-        Thread.sleep(1000)
-    }
-
     void createTestFile(testData) {
         logFile = new File("testLogfile.log")
         logFile.delete()
