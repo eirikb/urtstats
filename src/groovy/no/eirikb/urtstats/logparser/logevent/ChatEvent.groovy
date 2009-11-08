6dbe522e14ce09c4ad9c2e14c57762dc0e02eb5e
eirikb@katze <eirikb@katze>
2009-11-08 10:43:38 +0100

    Errr removed check again

--- src/groovy/no/eirikb/urtstats/logparser/logevent/ChatEvent.groovy
+++ src/groovy/no/eirikb/urtstats/logparser/logevent/ChatEvent.groovy
@@ -77,139 +77,137 @@ class ChatEvent extends Event {
 
     void command(player, cmd, message) {
         log.info "[ChatEvent] Command. Player: " + player + ". cmd: " + cmd + ". message: " + message
-        if (message != null) {
-            switch (cmd) {
-                case "help":
-                RCon.rcon("tell " + player.getUrtID() + " \"^Commands: level, pin\"")
-                break
+        switch (cmd) {
+            case "help":
+            RCon.rcon("tell " + player.getUrtID() + " \"^Commands: level, pin\"")
+            break
 
-                case "lvl":
-                case "level":
-                def p = player
-                if (message != null) {
-                    p = Player.findByNickIlikeAndUrtIDGreaterThanEquals('%' + message + '%', 0)
-                    if (p == null) {
-                        p = Player.findByNickIlike('%' + message + '%')
-                    }
+            case "lvl":
+            case "level":
+            def p = player
+            if (message != null) {
+                p = Player.findByNickIlikeAndUrtIDGreaterThanEquals('%' + message + '%', 0)
+                if (p == null) {
+                    p = Player.findByNickIlike('%' + message + '%')
                 }
-                if (p != null) {
-                    RCon.rcon("say \"^2" + p.getColorNick() + " ^7Level: ^1" + p.getLevel() + "\"")
-                } else{
-                    RCon.rcon("tell " + player.getUrtID() + " \"^7Player not found.\"")
-                }
-                break
+            }
+            if (p != null) {
+                RCon.rcon("say \"^2" + p.getColorNick() + " ^7Level: ^1" + p.getLevel() + "\"")
+            } else{
+                RCon.rcon("tell " + player.getUrtID() + " \"^7Player not found.\"")
+            }
+            break
 
-                case "status":
-                case "stats":
-                def p = player
-                if (message != null) {
-                    p = Player.findByNickIlikeAndUrtIDGreaterThanEquals('%' + message + '%', 0)
-                    if (p == null) {
-                        p = Player.findByNickIlike('%' + message + '%')
-                    }
+            case "status":
+            case "stats":
+            def p = player
+            if (message != null) {
+                p = Player.findByNickIlikeAndUrtIDGreaterThanEquals('%' + message + '%', 0)
+                if (p == null) {
+                    p = Player.findByNickIlike('%' + message + '%')
                 }
-                if (p != null) {
-                    def text = "say \"^2" + player.getColorNick() +
+            }
+            if (p != null) {
+                def text = "say \"^2" + player.getColorNick() +
                     " ^7Level: ^1" + player.getLevel() +
                     " ^7kills: ^1"  + Kill.countByKiller(player) +
                     " ^7Ratio: ^1"  + new DecimalFormat("#,###.##").format(PlayerTool.getTotalRatio(p)) +
                     " ^7NextLevel in: ^1" + (player.getNextlevel() - player.getLevel()) +
                     '"'
-                    RCon.rcon(text)
-                } else{
-                    RCon.rcon("tell " + player.getUrtID() + " \"^7Player not found.\"")
-                }
-                break
-
-                case "lol":
-                if (isAdmin(player)) {
-                    RCon.rcon("bigtext \"^2HAHAHA ^3HAAAA ^4HAHA HA ^6THAT IS ^5SOOO ^2FUNNEH!\"")
-                }
-                break
-
-                case "kick":
-                def clos = {RCon.rcon("clientkick " + it.getUrtID())}
-                rconCommand(player, cmd, message, clos)
-                break
-
-                case "pin":
-                RCon.rcon("tell " + player.getUrtID() + " \"^7Your PIN is: " + player.getPin() + "\"")
-                break
-
-                case "bigtext":
-                if (isPermitted(player, "bigtext")) {
-                    RCon.rcon("bigtext \"" + message + '"')
-                }
-                break
-
-                case "slap":
-                cmd = "slap"
-                def space = message.indexOf(' ')
-                def times = 1
-                if (space > 0) {
-                    try {
-                        times = Integer.parseInt(message.substring(space + 1))
-                    } catch (NumberFormatException e) {
-                        times = 1
-                    }
-                    message = message.substring(0, space)
-                }
-                def clos = {RCon.rcon(cmd + ' ' + it.getUrtID())}
-                for (i in 1..times) {
-                    rconCommand(player, cmd, message, clos)
-                }
-                break
-
-                case "forceteam":
-                case "move":
-                cmd = "forceteam"
-                def clos = {
-                    if (it.getTeam().getUrtID() == 1) {
-                        RCon.rcon(cmd + ' ' + it.getUrtID() + " blue")
-                    } else {
-                        RCon.rcon(cmd + ' ' + it.getUrtID() + " red")
-                    }
-                }
-                rconCommand(player, "forceteam", message, clos)
-                break
-
-                case "fixteams":
-                case "fix":
-                cmd = "fixteams"
-                if (isPermitted(player, "fixteams")) {
-                    def players = Player.findAllByUrtIDGreaterThanEquals(0)
-                    def red = 0
-                    def blue = 0
-                    players.each {
-                        it.getTeam().getUrtID() == 1 ? red++ : blue++
-                    }
-                    if (Math.max(red, blue) - Math.min(red, blue) > 1) {
-                        def moveFrom = red > blue ? 1 : 2
-                        def moveTo = moveFrom == 1 ? 2 : 1
-                        def movePlayers = []
-                        players.each {
-                            if (Math.max(red, blue) - Math.min(red, blue) > 1) {
-                                if (it.getTeam().getUrtID() == move) {
-                                    movePlayers.add(it)
-                                    red > blue ? red-- : blue++
-                                }
-                            }
-                        }
-                        movePlayers.each {
-                            def color = "red"
-                            if (it.getTeam().getUrtID() == 2) {
-                                color = blue
-                            }
-                            RCon.rcon("forceteam " + it.getUrtID() + ' ' + color)
-                        }
-                    }
-                }
-                break
-
-                case "say":
-                RCon.rcon("say \"^7" + player.getColorNick() + ": " + message + '"')
-                break
+                RCon.rcon(text)
+            } else{
+                RCon.rcon("tell " + player.getUrtID() + " \"^7Player not found.\"")
             }
+            break
+
+            case "lol":
+            if (isAdmin(player)) {
+                RCon.rcon("bigtext \"^2HAHAHA ^3HAAAA ^4HAHA HA ^6THAT IS ^5SOOO ^2FUNNEH!\"")
+            }
+            break
+
+            case "kick":
+            def clos = {RCon.rcon("clientkick " + it.getUrtID())}
+            rconCommand(player, cmd, message, clos)
+            break
+
+            case "pin":
+            RCon.rcon("tell " + player.getUrtID() + " \"^7Your PIN is: " + player.getPin() + "\"")
+            break
+
+            case "bigtext":
+            if (isPermitted(player, "bigtext")) {
+                RCon.rcon("bigtext \"" + message + '"')
+            }
+            break
+
+            case "slap":
+            cmd = "slap"
+            def space = message.indexOf(' ')
+            def times = 1
+            if (space > 0) {
+                try {
+                    times = Integer.parseInt(message.substring(space + 1))
+                } catch (NumberFormatException e) {
+                    times = 1
+                }
+                message = message.substring(0, space)
+            }
+            def clos = {RCon.rcon(cmd + ' ' + it.getUrtID())}
+            for (i in 1..times) {
+                rconCommand(player, cmd, message, clos)
+            }
+            break
+
+            case "forceteam":
+            case "move":
+            cmd = "forceteam"
+            def clos = {
+                if (it.getTeam().getUrtID() == 1) {
+                    RCon.rcon(cmd + ' ' + it.getUrtID() + " blue")
+                } else {
+                    RCon.rcon(cmd + ' ' + it.getUrtID() + " red")
+                }
+            }
+            rconCommand(player, "forceteam", message, clos)
+            break
+
+            case "fixteams":
+            case "fix":
+            cmd = "fixteams"
+            if (isPermitted(player, "fixteams")) {
+                def players = Player.findAllByUrtIDGreaterThanEquals(0)
+                def red = 0
+                def blue = 0
+                players.each {
+                    it.getTeam().getUrtID() == 1 ? red++ : blue++
+                }
+                if (Math.max(red, blue) - Math.min(red, blue) > 1) {
+                    def moveFrom = red > blue ? 1 : 2
+                    def moveTo = moveFrom == 1 ? 2 : 1
+                    def movePlayers = []
+                    players.each {
+                        if (Math.max(red, blue) - Math.min(red, blue) > 1) {
+                            if (it.getTeam().getUrtID() == move) {
+                                movePlayers.add(it)
+                                red > blue ? red-- : blue++
+                            }
+                        }
+                    }
+                    movePlayers.each {
+                        def color = "red"
+                        if (it.getTeam().getUrtID() == 2) {
+                            color = blue
+                        }
+                        RCon.rcon("forceteam " + it.getUrtID() + ' ' + color)
+                    }
+                }
+            }
+            break
+
+            case "say":
+            RCon.rcon("say \"^7" + player.getColorNick() + ": " + message + '"')
+            break
         }
     }
 
